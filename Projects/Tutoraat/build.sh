29f95098b277ad1a488e989660f9e07b73ab78a0
LennyTaelman <lenny.taelman@gmail.com>
2025-08-18 15:51:45 +0200

    update build.sh script

--- Projects/Tutoraat/build.sh
+++ Projects/Tutoraat/build.sh
@@ -5,7 +5,15 @@
 # Operate in the directory where this file is located
 cd $(dirname $0)
 
-git clone https://github.com/LennyTaelman/LeanTutoraat .
+# If directory has git content, pull; otherwise clone
+if [ -d ".git" ]; then
+    git pull
+else
+    # Remove everything except build.sh and clone
+    find . -maxdepth 1 -not -name 'build.sh' -not -name '.' -exec rm -rf {} \;
+    git clone https://github.com/LennyTaelman/LeanTutoraat .
+fi
+
 lake exe cache get
 lake build
 
