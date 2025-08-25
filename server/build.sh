d6518d0da8786ba7def1d67df7add756041622d4
LennyTaelman <lenny.taelman@gmail.com>
2025-08-25 16:11:02 +0200

    fix: project name change from Tutoraat to LeanTutoraat

--- server/build.sh
+++ server/build.sh
@@ -3,15 +3,15 @@
 # Navigate to Projects directory
 cd "$(dirname $0)/../Projects"
 
-echo "Building Tutoraat project"
+echo "Building LeanTutoraat project"
 
 # Clone the LeanTutoraat repository
-git clone https://github.com/LennyTaelman/LeanTutoraat Tutoraat
-cd Tutoraat
+git clone https://github.com/LennyTaelman/LeanTutoraat LeanTutoraat
+cd LeanTutoraat
 
 # Build the project
 echo "Getting cache and building..."
 lake exe cache get
 lake build
 
-echo "Tutoraat build completed"
+echo "LeanTutoraat build completed"
