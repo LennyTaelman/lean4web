3b8af0a88cda4b58295968276520ac102ae56466
LennyTaelman <lenny.taelman@gmail.com>
2025-09-02 17:21:19 +0200

    run_image script now stops running containers first

--- docker-tools/run_image.sh
+++ docker-tools/run_image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# now run the latest docker image
-docker run -d --restart=unless-stopped -p 8080:8080 tutoraat_web
+# Stop all running containers
+docker stop $(docker ps -q) 2>/dev/null || true
+
+# Remove all stopped containers
+docker rm $(docker ps -aq) 2>/dev/null || true
+
+# Now run the latest docker image
+docker run -d --name tutoraat_web --restart=unless-stopped -p 8080:8080 tutoraat_web
